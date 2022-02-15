a61a1faa041d8e5c09bfd770a703e930bf38ff17
Gioele Fezzuoglio <gfezzuog@e3r4p5.42roma.it>
2022-02-15 15:23:19 +0100

    Finished project

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfezzuog <gfezzuog@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/03 19:42:46 by gfezzuog          #+#    #+#             */
+/*   Updated: 2022/02/03 19:42:46 by gfezzuog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	char			*ptr;
+
+	ptr = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr[i] == c)
+			return ((unsigned char *)&ptr[i]);
+		i++;
+	}
+	return (NULL);
+}
