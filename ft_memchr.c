20537f1b712c583fc85e6b785cfe59dc75ff7ce1
Gioele Fezzuoglio <gfezzuog@e3r4p5.42roma.it>
2022-02-15 17:00:49 +0100

    Push2

--- ft_memchr.c
+++ ft_memchr.c
@@ -22,7 +22,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (ptr[i] == c)
-			return ((unsigned char *)&ptr[i]);
+			return ((void *)(ptr + i));
 		i++;
 	}
 	return (NULL);
